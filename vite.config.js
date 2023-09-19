f4884853a7182d43a8d8390fcba9c4c711bdc3f7
PreethiTapati <preetimadhuri22@gmail.com>
2023-09-19 16:21:19 +0200

    added some styles

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-get-a-joke",
+   build: {
+    chunkSizeWarningLimit: 1600
+  },
 })
